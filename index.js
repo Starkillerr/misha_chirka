37e262415a28965d050bad431222b8073f006ad7
OLEKSANDR BRASLAVSKYI <brasal07@gmail.com>
2026-02-09 00:12:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 const menu = document.getElementById('menu');
 const burger = document.getElementById('burger');
 const close = document.getElementById('close');
+const overlay = document.getElementById("overlay");
 
 burger.addEventListener('click', e => {
   e.preventDefault();
   menu.classList.add('active');
   document.documentElement.classList.add('menu-open');
+  overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 close.addEventListener('click', e => {
@@ -20,15 +27,15 @@ const cartPopup = document.getElementById("cartPopup");
 const cartClose = document.getElementById("cartClose");
 
 cartBtn.addEventListener("click", () => {
-    cartPopup.style.display = "flex"; 
+    cartPopup.style.display = "flex";  
 });
 
 cartClose.addEventListener("click", () => {
-    cartPopup.style.display = "none"; 
+    cartPopup.style.display = "none";  
 });
 
 cartPopup.addEventListener("click", (e) => {
     if(e.target === cartPopup) {
-        cartPopup.style.display = "none"; 
+        cartPopup.style.display = "none";  
     }
 });
